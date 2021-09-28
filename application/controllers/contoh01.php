d86f3a51f379a73ffe575b414d7e0403c5163c1c
rofifr <whoexaltsjune25@gmail.com>
2021-09-28 18:59:03 +0700

    menambahkan framework

---
+++ application/controllers/contoh01.php
@@ -0,0 +1,13 @@
+<?php
+class contoh01 extends CI_controller
+{
+public function index ()
+{
+    echo"<hl>perkenalan</h1>";
+    echo"Nama sara ROFI FATURROHMAN
+         saya tinggal di daerah Jatiwaras
+         hobby saya adalah editing
+         no telp 085223464039":
+         }
+    }
+?>
